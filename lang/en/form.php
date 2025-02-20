2ac7fa308458c9ee4500dc921798d96e1f724519
n-schouten <2003.nielsschouten@gmail.com>
2025-02-20 15:10:24 +0100

    Create form.php

---
+++ lang/en/form.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'image'     => 'Image',
+    'name'      => 'Name',
+    'alt'       => 'Alt',
+    'title'     => 'Title',
+    'author'    => 'Author',
+    'source'    => 'Source'
+];
