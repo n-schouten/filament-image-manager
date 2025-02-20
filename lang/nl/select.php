e03342b9cb30063e9331d668b40012ac6c52cbf0
n-schouten <2003.nielsschouten@gmail.com>
2025-02-20 15:12:02 +0100

    Create select.php

---
+++ lang/nl/select.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'title'         => 'Selecteer een afbeelding',
+    'description'   => 'Klik op een afbeelding om deze te selecteren.',
+    'none_found'    => 'Je hebt nog geen afbeeldingen geupload.'
+];
