5ab07c878ba90d04e25caaab98cfa34a2431830c
n-schouten <2003.nielsschouten@gmail.com>
2025-02-20 15:10:05 +0100

    Create actions.php

---
+++ lang/en/actions.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'upload'        => 'Upload Image',
+    'select'        => 'Select Image',
+    'choose_file'   => 'Choose File',
+    'finalize'      => 'Finalize Upload & Save',
+    'cancel'        => 'Cancel',
+    'remove'        => 'Remove'
+];
