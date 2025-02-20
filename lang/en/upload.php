b9c30f4c2ec9e5f58dc10e4daefe19e15d844ad0
n-schouten <2003.nielsschouten@gmail.com>
2025-02-20 15:11:01 +0100

    Create upload.php

---
+++ lang/en/upload.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'no_preview'        => 'No Preview',
+    'selected'          => 'Selected:',
+    'click_to_change'   => 'Click To Change',
+    'uploading'         => 'Uploading:',
+    'uploading_button'  => 'Busy Uploading'
+];
