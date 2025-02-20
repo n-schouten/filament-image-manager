4d72a01109843fde6488455fba935ee78e3c8070
n-schouten <2003.nielsschouten@gmail.com>
2025-02-20 15:11:21 +0100

    Create actions.php

---
+++ lang/nl/actions.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'upload'        => 'Afbeelding uploaden',
+    'select'        => 'Afbeelding selecteren',
+    'choose_file'   => 'Bestand kiezen',
+    'finalize'      => 'Uploaden afronden & opslaan',
+    'cancel'        => 'Annuleren',
+    'remove'        => 'Verwijderen'
+];
