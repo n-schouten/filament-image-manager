5242366bf50a4b87bc51315847c57c2a7acedbd4
n-schouten <2003.nielsschouten@gmail.com>
2025-02-20 15:10:48 +0100

    Create select.php

---
+++ lang/en/select.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'title'         => 'Select an image',
+    'description'   => 'Click on an image to select it.',
+    'none_found'    => 'You didn\'t upload any image yet.'
+];
