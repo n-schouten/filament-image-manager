95b50391e48217c07e555043821d90c0f6571e0e
n-schouten <2003.nielsschouten@gmail.com>
2025-02-20 15:12:13 +0100

    Create upload.php

---
+++ lang/nl/upload.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'no_preview'        => 'Geen voorbeeld',
+    'selected'          => 'Geselecteerd:',
+    'click_to_change'   => 'Klik om te veranderen',
+    'uploading'         => 'Uploaden:',
+    'uploading_button'  => 'Aan het uploaden'
+];
